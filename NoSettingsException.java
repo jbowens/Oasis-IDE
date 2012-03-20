6220bec9ebe6f040fecfe8338861cf32d86a2590
Jackson Owens <jackson_owens@brown.edu>
2012-03-20 00:45:18 -0400

    Commented NoSettingsException

--- NoSettingsException.java
+++ NoSettingsException.java
@@ -1,3 +1,7 @@
 package Camel;
 
+/*
+ * A NoSettingsException is thrown when settings are unable to be loaded by
+ * the Config class. It can be viewed as irrecoverable.
+ */
 public class NoSettingsException extends Exception {}
