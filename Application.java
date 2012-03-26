49e3b656003f89dd49e317f72c9c93b848f37c52
Jackson Owens <jackson_owens@brown.edu>
2012-03-25 22:10:56 -0400

    Added a main Application class and a Runner class

---
+++ Application.java
@@ -0,0 +1,53 @@
+package Camel;
+
+import Camel.Interactions.*;
+import java.util.*;
+
+/*
+ * Primary application class. Instantation of this class creates
+ * a new instance of the application, including a new GUI and a 
+ * new backend.
+ */
+public class Application {
+
+    protected Config config;
+    protected InteractionsManager interactionsManager;
+
+    public Application(String settingsFile) throws NoSettingsException {
+        // TODO: Add test to ensure OCaml is installed / find the ocaml executable
+        this.config = new Config( settingsFile );
+        this.interactionsManager = new InteractionsManager("ocaml");
+        setupGui();
+    }
+
+    /*
+     * Constructs the front-end gui for the application.
+     */
+    protected void setupGui() {
+        // TODO: Connect with Raymond's GUI code
+    }
+
+    /*
+     * Opens a file.
+     */ 
+    public void openFile( String filename ) {
+        // TODO: Implement
+    }
+
+    /*
+     * Opens several files at once.
+     */
+    public void openFiles( List<String> filenames ) {
+        for( String file : filenames )
+            this.openFile( file );
+    }
+
+    /*
+     * Creates a new blank file.
+     */
+    public void openBlankProgram() {
+        // TODO: Implement
+    }
+
+}
+
---
+++ Runner.java
@@ -0,0 +1,37 @@
+package Camel;
+
+import java.util.ArrayList;
+
+/*
+ * Contains the main() method for the entire application. This is the class that
+ * should be run to launch execution.
+ */
+public class Runner {
+
+    public static void main(String args[]) {
+
+        /* Look for files to open */
+        ArrayList<String> filesToOpen = new ArrayList<String>();
+        for( int i = 0; i < args.length; i++ ) {
+            filesToOpen.add( args[i] );
+        }
+
+        /* Create the new instance of the application */
+        try {
+            String settingsFile = "settings.xml";
+            Application app = new Application( settingsFile );
+
+            /* Load the files, or a blank one */
+            if( ! filesToOpen.isEmpty() )
+                app.openFiles( filesToOpen );
+            else
+                app.openBlankProgram();
+
+        } catch(NoSettingsException e) {
+            // TODO: Initialize a new settings file with default settings.
+            System.err.println("Unable to find or load application settings.");
+            System.exit(1);
+        }
+    }
+
+}
