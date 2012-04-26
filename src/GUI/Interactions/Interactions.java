b8f0c6cbcbe57c2ca6906104214381595b90c6ef
raymondhan1991 <raymond.han1991@gmail.com>
2012-04-26 18:14:56 -0400

    worked on interactions

--- src/GUI/Interactions/Interactions.java
+++ src/GUI/Interactions/Interactions.java
@@ -1,16 +1,16 @@
-package Interactions;
+package Camel.GUI.Interactions;
 
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-
+import Camel.Interactions.*;
 import java.awt.GridBagLayout;
 
 public class Interactions extends JPanel{
 	protected JTabbedPane tabs;
 	InteractionsManager _im;
-	public InteractionsWindow(InteractionsManager im)
+	public Interactions(InteractionsManager im)
 	{
 		super(new GridBagLayout());
 		tabs = new JTabbedPane();
@@ -21,11 +21,21 @@ public class Interactions extends JPanel{
 		add(tabs,fullFill);
 		setSize(600,600);
 		_im = im;
-		makeTab("");
+		makeTab(null);
 	}
 	public void makeTab(String filePath) {
 		Tab t = new Tab(_im,filePath);
-		tabs.addTab(fh.getName(), t);
+		tabs.addTab("int window", t);
+	}
+
+	public static void main(String[] args)
+	{
+		JFrame f = new JFrame();
+		InteractionsManager im = new InteractionsManager("ocaml");
+		Interactions iw = new Interactions(im);
+		f.add(iw);
+		f.pack();
+		f.setVisible(true);
 	}
 	
 }
--- src/GUI/Interactions/Tab.java
+++ src/GUI/Interactions/Tab.java
@@ -1,19 +1,20 @@
-package Interactions;
+package Camel.GUI.Interactions;
 
 import java.awt.BorderLayout;
 import java.io.File;
-
+import java.io.FileNotFoundException;
+import java.awt.event.*;
 import javax.swing.JEditorPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-
-import BackEnd.FileHandler;
+import Camel.Interactions.*;
 
 public class Tab extends JPanel implements TextOutputListener {
 
 	JEditorPane textPane;
 	InteractionsManager _im;
 	int _handle;
+	String query;
 
 	public Tab(InteractionsManager im, String filePath) {
 		textPane = new JEditorPane();
@@ -21,17 +22,50 @@ public class Tab extends JPanel implements TextOutputListener {
 		setLayout(new BorderLayout());
 		add(sc);
 		this._im = im;
-		_handle = _im.newInteractionsInstance(filePath);
+		textPane.addKeyListener(new KListener());
+		try {
+			_handle = _im.newInteractionsInstance(filePath);
+			_im.registerOutputListener(this, _handle);
+		} catch(Exception e) {}
 
 	}
 	public void receiveOutput(TextOutputEvent evt)
 	{
-		textPane.setText(textPane.getText() + evt.getText());	
+		textPane.setText(textPane.getText() + evt.getText());
+        textPane.setCaretPosition(textPane.getText().length());	
 	}
 	public String getText()
 	{
 		return textPane.getText();
 	}
-	
+	public class KListener implements KeyListener
+	{
+		public KListener()
+		{
+			
+		}
+		public void keyTyped(KeyEvent e) {
+			try {
+				System.out.println("Sending " + e.getKeyChar());
+				if(e.getKeyChar() == '\b' && counter > 0)
+        		{
+        			
+        		}
+        		_im.processUserInput(_handle, e.getKeyChar());
+        		
+
+        	} catch(Exception e2) {}
+   		}
+
+	    /** Handle the key-pressed event from the text field. */
+	    public void keyPressed(KeyEvent e) {
+	    }
+
+	    /** Handle the key-released event from the text field. */
+	    public void keyReleased(KeyEvent e) {
+	    }
+
+	}
+
 	
 }
