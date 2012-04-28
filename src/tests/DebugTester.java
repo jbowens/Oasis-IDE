83c3efb0af71daac38f1ffebdf02501171f906d7
aleblang <aleblang@cs.brown.edu>
2012-04-28 14:57:13 -0400

    debugger stuff

---
+++ src/tests/DebugTester.java
@@ -0,0 +1,17 @@
+package camel.tests;
+
+import java.io.*;
+import camel.debug.*;
+
+public class DebugTester{
+	public static void main(String[] args) throws IOException{
+		Debug myDebug = new Debug("/test-ml");
+		assert(myDebug != null);
+		//if(myDebug != null){
+		//	System.out.println("Not null");
+		//}
+		myDebug.getStartInfo();
+		myDebug.callDebug();
+
+	}
+}
