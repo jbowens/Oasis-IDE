0b4e267196931f47dd03a63c4eeb021ab62287b8
Jackson Owens <jackson_owens@brown.edu>
2012-05-20 20:32:23 -0400

    Cleaned things up

--- listlength.ml
+++
@@ -1,7 +0,0 @@
-
-(* myListLength
-   Input: a list, aList
-   Output: the number of items in aList *)
-let rec myListLength( aList : 'a list) : int = match aList with
- | [] -> 0
- | _::tl -> 1 + myListLength tl;;
