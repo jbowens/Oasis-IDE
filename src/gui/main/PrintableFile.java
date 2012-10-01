46a955b70cb876669c110896259d4ea41499a5cb
Jackson Owens <jackson_owens@brown.edu>
2012-09-30 20:57:00 -0400

    Commented PrintableFile

--- src/gui/main/PrintableFile.java
+++ src/gui/main/PrintableFile.java
@@ -2,6 +2,13 @@ package camel.gui.main;
 
 import java.io.File;
 
+/**
+ * A File whose toString() method is overriden to print out
+ * just the file's filename and not its entire path.
+ *
+ * @author jbowens
+ * @since September 2012
+ */
 public class PrintableFile extends File
 {
 
@@ -16,6 +23,12 @@ public class PrintableFile extends File
         return getName();
     }
 
+    /**
+     * Creates a PrintableFile from a regular file.
+     *
+     * @param f  the file to convert
+     * @return a PrintableFile that represents the same file
+     */
     public static PrintableFile createFromFile(File f)
     {
         return new PrintableFile( f.getAbsolutePath() );
